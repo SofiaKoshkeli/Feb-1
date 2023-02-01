edd348a9d9acc5e4550075a2a2c32abe5cfaf619
SofiaKoshkeli <sofokoshkelov@gmail.com>
2023-02-01 23:47:31 +0400

    something

---
+++ script.js
@@ -0,0 +1,14 @@
+let students = [
+    {name: "gio", age: 19},
+    { name:"tedo", age:25},
+    { name:"sopo", age:18}
+
+];
+function newStudents(students){
+    for ( i= 0; i <= students.length; i++);
+   
+}
+newStudents(students);
+
+
+
